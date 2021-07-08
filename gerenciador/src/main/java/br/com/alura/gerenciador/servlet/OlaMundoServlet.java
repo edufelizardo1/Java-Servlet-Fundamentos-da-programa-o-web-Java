5ad5179cb9d01006b5e601820f97c3a20ea783c3
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-07-08 19:48:25 -0300

    Create OlaMundoServlet.java

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/OlaMundoServlet.java
@@ -0,0 +1,14 @@
+package br.com.alura.gerenciador.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class OlaMundoServlet extends HttpServlet {
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) {
+		
+	}
+
+}
