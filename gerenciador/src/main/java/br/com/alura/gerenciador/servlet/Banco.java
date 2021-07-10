6ef044d7e8bb1d89ed03e61f571185abc8f26583
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-07-10 15:47:18 -0300

    Update Banco.java

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Banco.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/Banco.java
@@ -7,6 +7,16 @@ public class Banco {
 
 	private static List<Empresa> empresas = new ArrayList<>();
 	
+	static {
+		Empresa empresa1 = new Empresa();
+		empresa1.setNome("Start-Up");
+		Empresa empresa2 = new Empresa();
+		empresa2.setNome("Negócio");
+		
+		empresas.add(empresa1);
+		empresas.add(empresa2);
+	}
+	
 	public void adicona(Empresa empresa) {
 		empresas.add(empresa);
 	}
