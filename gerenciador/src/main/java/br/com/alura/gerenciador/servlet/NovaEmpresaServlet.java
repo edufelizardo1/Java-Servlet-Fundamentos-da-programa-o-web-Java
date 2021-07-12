46f269e80a5b0489bac6dce9c3638da12d764414
Eduardo Felizardo Cândido <41344070+edufelizardo1@users.noreply.github.com>
2021-07-12 09:35:53 -0300

    Update NovaEmpresaServlet.java

--- gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -30,6 +30,7 @@ public class NovaEmpresaServlet extends HttpServlet {
 		
 		//chamar o JSP
 		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+		request.setAttribute("empresa", empresa.getNome());
 		rd.forward(request, response);
 	}
 	
